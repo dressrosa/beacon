f46810cebacaffaff8446a359b39f92c6cac7b75
dressrosa <1546428286@qq.com>
2019-12-08 18:10:22 +0800

    fix localhost & mvn package resources

--- beacon-common/src/main/java/com/xiaoyu/beacon/common/utils/NetUtil.java
+++ beacon-common/src/main/java/com/xiaoyu/beacon/common/utils/NetUtil.java
@@ -5,7 +5,8 @@
 package com.xiaoyu.beacon.common.utils;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 /**
  * @author hongyu
@@ -15,12 +16,35 @@ import java.net.UnknownHostException;
 public class NetUtil {
 
     public static String localIP() {
-        InetAddress addr;
         try {
-            addr = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            return null;
+            InetAddress addr = null;
+            // 遍历网络接口
+            Enumeration<NetworkInterface> nifs = NetworkInterface.getNetworkInterfaces();
+            while (nifs.hasMoreElements()) {
+                NetworkInterface iface = nifs.nextElement();
+                // 遍历ip
+                Enumeration<InetAddress> iads = iface.getInetAddresses();
+                while (iads.hasMoreElements()) {
+                    InetAddress inetAddr = iads.nextElement();
+                    // 排除loopback类型地址
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // site-local地址
+                            return inetAddr.getHostAddress();
+                        } else if (addr == null) {
+                            addr = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (addr != null) {
+                return addr.getHostAddress();
+            }
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return addr.getHostAddress();
+        return null;
+
     }
 }
